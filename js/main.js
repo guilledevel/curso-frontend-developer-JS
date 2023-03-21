58db190f70aec51df2a6cd7e0f4bffbfc58e0e27
Guillermo <ocana.alborta.guillermo@gmail.com>
2023-03-20 23:56:25 -0400

    menu mobile

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
 
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
-menuEmail.addEventListener('click', togleDesktopMenu);
+menuEmail.addEventListener('click', toggleDesktopMenu);
+menuHamIcon.addEventListener('click', toggleMobileMenu);
 
-function togleDesktopMenu() {
+function toggleDesktopMenu() {
+    // Toggle the "inactive" class on the desktop menu
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    // Toggle the "inactive" class on the mobile menu
+    mobileMenu.classList.toggle('inactive');
 }
